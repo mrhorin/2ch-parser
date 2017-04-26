51aa10823ca7dc7c0ebf8ea7e58cbc27f891b8ca
mrhorin <u0813mb@gmail.com>
2017-04-27 07:30:53 +0900

    BoardとThreadをモジュール化

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import Board from 'js/board'
 import Thread from 'js/thread'
 
-var board = new Board("http://jbbs.shitaraba.net/internet/22724/")
-// var thread = new Thread(board.threads[0].url)
-
-console.log(board.threads)
+module.exports = {
+  Board: Board,
+  Thread: Thread
+}
