4b4e2c4aea9913a7b73e3c67bfb6d7d1ee8b0bc5
mrhorin <u0813mb@gmail.com>
2017-05-11 19:45:56 +0900

    UrlParserをrequireできるように

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import Board from 'js/board'
 import Thread from 'js/thread'
+import UrlParser from 'js/url_parser'
 
 module.exports = Board
 
 module.exports.Board = Board
 
 module.exports.Thread = Thread
+
+module.exports.UrlParser = UrlParser
